c8984bf3725fb793e3a4ef01963868700c60feec
Matt Bianco <matthewbianco@protonmail.com>
2021-04-05 15:41:14 -0400

    added user models

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  port: 3306
+});
+console.log("connected to database");
+module.exports = sequelize;
