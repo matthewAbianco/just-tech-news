b15099eca9e0d6ee97e4b2be7befcedb7cf895e1
Matt Bianco <matthewbianco@protonmail.com>
2021-04-07 11:28:57 -0400

    added post routes

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,12 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-
-// create connection to our database, pass in your MySQL information for username and password
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: '127.0.0.1',
+  host: 'localhost',
   dialect: 'mysql',
   port: 3306
 });
-console.log("connected to database");
+
 module.exports = sequelize;
